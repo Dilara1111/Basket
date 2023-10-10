a38eccfcf1ee7afd56a5bd6a1a2c721ceccb02af
HuseynovaDilara <huseynova.8885@gmail.com>
2023-10-10 20:44:03 +0400

    Basket added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,35 @@
+let cart = {
+    'pls11' : 2,
+    'mns22' : 2,
+}
 
+document.onclick = event => {
+    if (event.target.classList.contains('plus')){
+       plusFunction(event.target.clas);    
+    }
+    else if (event.target.classList.contains('minus')){
+        plusFunction(event.target.clas); 
+     }
+}
+
+const plusFunction = id => {
+    cart[id]++;
+    renderCart();
+}
+
+const minusFunction = id => {
+    if (cart[id]-1 ==0){
+        deleteFunction;
+        return true;
+    }
+    cart[id]--;
+    renderCart();
+}
+const deleteFunction = id =>{
+    delete cart[id];
+    renderCart();
+}
+const renderCart = () => {
+    console.log(cart);
+}
+renderCart()
